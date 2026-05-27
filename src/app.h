68175ee413f0328e5d241898686439ddd4bc3284
FreeFalling_Snow <guo.yunzhe@outlook.com>
2026-05-27 19:02:43 +0800

    按钮间距gap改为2px

--- src/app.h
+++ src/app.h
@@ -6625,7 +6625,7 @@ private:
     {
         RECT handle = GetWidgetMoveHandleRect(widget);
         constexpr int btnSize = 14;
-        constexpr int gap = 6;
+        constexpr int gap = 2;
         constexpr int resizeReserve = 24;
         return MakeRect(handle.right - resizeReserve - gap - btnSize - gap - btnSize, handle.top + 5,
             handle.right - resizeReserve - gap - btnSize - gap, handle.bottom - 3);
@@ -6635,7 +6635,7 @@ private:
     {
         RECT handle = GetWidgetMoveHandleRect(widget);
         constexpr int btnSize = 14;
-        constexpr int gap = 6;
+        constexpr int gap = 2;
         constexpr int resizeReserve = 24;
         return MakeRect(handle.right - resizeReserve - gap - btnSize, handle.top + 5,
             handle.right - resizeReserve - gap, handle.bottom - 3);
@@ -6645,7 +6645,7 @@ private:
     {
         RECT handle = GetWidgetMoveHandleRect(widget);
         constexpr int btnSize = 14;
-        constexpr int gap = 6;
+        constexpr int gap = 2;
         constexpr int resizeReserve = 24;
         return MakeRect(handle.right - resizeReserve - gap - btnSize, handle.top + 5,
             handle.right - resizeReserve - gap, handle.bottom - 3);
